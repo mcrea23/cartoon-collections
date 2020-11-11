f3b48f5953a15e1244087c505494852a6dc4021b
+ <mcrea23@aol.com>
2020-11-11 16:56:18 -0600

    commit tree cf16dd6962ddb7c939d39e7950a309a738f91a88

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  #dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  names.each_with_index do | name , index |
+    puts "#{index + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(things)# code an argument here
   # Your code here
+  # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+    things.collect do |call| 
+    call.capitalize + "!"
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(x)
+  x.any? { |call| call.length > 4 }
+  end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
+def find_the_cheese(yum)# code an argument here
+# the array below is here to help
+cheese_types = ["cheddar", "gouda", "camembert"]
+lunch_menu.each do |lunch_item|
+  "#{lunch_item}!"
 end
